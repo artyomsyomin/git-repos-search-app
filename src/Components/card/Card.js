a2dea8d4b4930af5db6e65eb6057c3ff369145eb
Artyom <seminartem98@mail.ru>
2021-03-26 07:45:03 +0700

    changed stylyes

--- src/Components/card/Card.js
+++ src/Components/card/Card.js
@@ -4,10 +4,6 @@ import { Link } from 'react-router-dom';
 import './card.css';
 
 const Card = ({ reposInfo, loading }) => {
-  //   const visitPage = (url) => {
-  //     window.location.href = url;
-  //   };
-
   let cardInfo = '';
   !reposInfo.length
     ? (cardInfo = '')
@@ -15,13 +11,12 @@ const Card = ({ reposInfo, loading }) => {
         return (
           <div key={item.id} className="card">
             <Link className="repo-item" to={`/${item.full_name}`}>
-              <h3 className='repo-name'>{item.name}</h3>
-              <p className='repo-stars'><i className="fas fa-star"/> {item.stargazers_count}</p>
-              <p className='repo-commit'>Last update: {item.updated_at}</p>
-              <p className='repo-description'>{item.description}</p>
-              {/* <button onClick={visitPage(item.html_url)}>Visit Page Now</button> */}
-              {/* <p onClick={() => visitPage(item.html_url)}>Visit Page Now</p> */}
-              {/* <p onClick={() => onclick="window.location.href='otherlink'"}>Visit Page Now</p> */}
+              <h3 className="repo-name">{item.name}</h3>
+              <p className="repo-stars">
+                <i className="fas fa-star" /> {item.stargazers_count}
+              </p>
+              <p className="repo-commit">Last update: {item.updated_at}</p>
+              <p className="repo-description">{item.description}</p>
             </Link>
             <a className="git-link" href={item.html_url}>
               Go to GitHub
